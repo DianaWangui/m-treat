863644827b134620b5fa48e7f8103375739d54c0
DianaWangui <dianawanguikt048@gmail.com>
2025-01-27 14:25:45 +0300

    added url for the fronted be served by one server

--- BACKEND/accounts/urls.py
+++ BACKEND/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PatientRegistration, PatientLogin, UserProfileView
+from .views import PatientRegistration, PatientLogin, UserProfileView, UpdateUserProfileView
 
 urlpatterns = [
     path('register/', PatientRegistration.as_view(), name='patient_registration_api'),
     path('login/', PatientLogin.as_view(), name='patient_login_api'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('update-profile/', UpdateUserProfileView.as_view(), name='update-profile'),
+
 ]
