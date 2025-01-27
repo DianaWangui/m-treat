8d2c78e7c4782357b5fb4980890afba57a9248b2
DianaWangui <dianawanguikt048@gmail.com>
2025-01-27 14:19:32 +0300

    updated to serve all the redux states

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import { injectStore } from './axiosInterceptor';
 
 export const store = configureStore({
-  reducer: { user: userReducer },  
+  reducer: {
+    user: userReducer,
+  },
 });
+
+injectStore(store);
